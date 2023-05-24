6d849975b87df64c95087ed67516f6d37eac7184
Kristian Järventaus <kristian@clausal.com>
2023-05-24 08:35:20 +0300

    Accidentally called non-callable field Path.name

--- wikitextprocessor/dumpparser.py
+++ wikitextprocessor/dumpparser.py
@@ -126,7 +126,7 @@ def process_dump(
 def overwrite_pages(ctx: "Wtp", folder_paths: List[Path]) -> None:
     for folder_path in folder_paths:
         for file_path in folder_path.iterdir():
-            if file_path.name().startswith("."):
+            if file_path.name.startswith("."):
                 continue
             with file_path.open(encoding="utf-8") as f:
                 first_line = f.readline()
